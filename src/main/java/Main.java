47abb923c7411f29e1762cebf4c4f701ca3d97d3
Luisa Leon Campos <luisaleon34563@gmail.com>
2024-11-14 21:03:38 +0000

    subiendo proyecto

---
+++ src/main/java/Main.java
@@ -0,0 +1,13 @@
+
+
+public class Main {
+  public static void main(String[] args) {
+
+    double DineroTotal = 23.0;
+    double DineroRestante = 12.75;
+    double DineroGastado = DineroTotal - DineroRestante;
+    System.out.println("el dinero que Carmen gasto fue de: " + DineroGastado  + "€");
+  }
+
+  
+}
